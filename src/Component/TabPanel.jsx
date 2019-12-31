43f60e67b7bbaf7c22c7cf72760b2e012e7afac5
Brian <villarroelbrian1110@gmail.com>
2019-12-31 01:45:23 -0300

    se agregan componentes

---
+++ src/Component/TabPanel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Content/CSS/pepe.css';
+
+class TabPanel extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+    render(){
+        return(
+            <div className="tabpanel-content">
+
+            </div>
+        );
+    }
+}
+export default TabPanel;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import Application from './Application.jsx';
 
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-  }
-  
-  setInterval(tick, 1000);
+var root = document.getElementById('root');
+root.setAttribute('class','root-content')
+ReactDOM.render(<Application/>,root );
+ 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
