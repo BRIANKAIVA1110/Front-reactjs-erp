976755da2e6d98b32b52e7481d53c08e18c362bc
Brian <villarroelbrian1110@gmail.com>
2019-12-31 02:07:50 -0300

    se gregan componentes

--- src/Component/ProfilePanel.jsx
+++ src/Component/ProfilePanel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Content/CSS/pepe.css';
+import avatar from '../Content/Images/perfil-avatar-default.jpg';
 
 class ProfilePanel extends React.Component{
     constructor(props)
@@ -9,7 +10,9 @@ class ProfilePanel extends React.Component{
     render(){
         return(
             <div className="profilePanel-content">
-
+                <div className="profilePanel-content-avatar">
+                    <img className="profilePanel-content-avatar-img" src={avatar} alt="Avantar"/>
+                </div>
             </div>
         );
     }
